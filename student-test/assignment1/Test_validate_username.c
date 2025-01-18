1b8b34d26e4891a3853335f0206275464633e5f8
TheBrellis <brianroyalellis@gmail.com>
2025-01-18 08:44:54 -0500

    Test_validate_username.c fixed

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,14 +18,13 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-	bool user = myusername();
+    	bool user = my_username();
 	bool mal_user = malloc_username_from_conf_file();
-
-	if (user == mal_user) {
-		TEST_ASSERT_TRUE_MESSAGE(true);
-	else
-	    	TEST_ASSERT_TRUE_MESSAGE(false,"ASED students, please fix me!");
+	
+	if (user == mal_user){
+		TEST_ASSERT_TRUE_MESSAGE(true,"PASS");
+	}
+	else { 
+		TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 	};
-
-   /* TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");*/
 }
